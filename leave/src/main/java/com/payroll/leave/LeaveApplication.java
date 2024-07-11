434ea3dfe0b7abd2163d74ca113b6c24ab1ee6f7
shubh-xmate <shuklashubham2277@gmail.com>
2024-07-11 14:19:24 +0530

    added eurekaserver

--- leave/src/main/java/com/payroll/leave/LeaveApplication.java
+++ leave/src/main/java/com/payroll/leave/LeaveApplication.java
@@ -2,8 +2,10 @@ package com.payroll.leave;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class LeaveApplication {
 
 	public static void main(String[] args) {
