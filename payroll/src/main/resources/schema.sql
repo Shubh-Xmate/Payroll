7cd12bde0e3316120864d1327e3ed1c56f27a200
Parneet Singh <parneet.a.singh@ukg.com>
2024-07-12 17:06:52 +0530

    createforall payroll

--- payroll/src/main/resources/schema.sql
+++ payroll/src/main/resources/schema.sql
@@ -5,6 +5,12 @@ CREATE TABLE IF NOT EXISTS `salary` (
     `allowances` decimal(10,2) NOT NULL
 );
 
+INSERT INTO salary (basic_salary, hra, allowances) VALUES (150000.0, 45000.0, 20000.0);
+INSERT INTO salary (basic_salary, hra, allowances) VALUES (100000.0, 35000.0, 10000.0);
+--INSERT INTO salary (basic_salary, hra, allowances) VALUES (50000.0, 25000.0, 8000.0);
+--INSERT INTO salary (basic_salary, hra, allowances) VALUES (35000.0, 15000.0, 5000.0);
+--INSERT INTO salary (basic_salary, hra, allowances) VALUES (20000.0, 10000.0, 2000.0);
+
 CREATE TABLE if NOT EXISTS `payroll` (
     `payroll_id` int AUTO_INCREMENT PRIMARY KEY,
     `employee_id` int NOT NULL,
